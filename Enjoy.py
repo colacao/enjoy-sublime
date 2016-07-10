04721d5ae8c9a88763fd9558b47314179d16b497
colacao <caoyueqq@163.com>
2016-07-10 18:43:50 +0800

    edit

--- Enjoy.py
+++ Enjoy.py
@@ -31,39 +31,34 @@ class EnjoyCommand(sublime_plugin.TextCommand):
 				return checkenjoy(GetParentPath(path))
 
 		def on_done(name):
-			# for key, value in self.view.window().extract_variables().items():
-			# 	print(key,value)
-			# platform Windows
-			# file_extension html
-			# file_name index.html
-			# folder D:\wamp\www\360
-			# file_path C:\Users\colacao.CAOYUE\Desktop\Test\src\home
-			# file C:\Users\colacao.CAOYUE\Desktop\Test\src\home\index.html
-			# file_base_name index
-			# packages C:\Users\colacao.CAOYUE\AppData\Roaming\Sublime Text 3\Packages
-			dirs = self.view.window().extract_variables()['file_path']
-			enjoy = checkenjoy(dirs)
-			# if enjoy:
-			# 	sublime.message_dialog("找到enjoy目录"+enjoy)
-
-			sublime.message_dialog("当前文件=="+dirs+"\n"+"用户输入=="+name+"\n"+"enjoy目录=="+checkenjoy(dirs)+"\n"+"命令参数=="+args['id'])
+			desktop =  os.path.expanduser('~')	+"\Desktop"	
+			os.system("cd "+desktop+" "+" && "+"enjoy init " + name)
+			
 		 
 
 
-		dirs = self.view.window().extract_variables()['file_path']
-		enjoy = checkenjoy(dirs)
+		dirs = self.view.window().extract_variables()
+
+		
 		if (args['id']=="init"):
 			self.view.window().show_input_panel('请输入项目名称','Test',on_done,on_change,on_cancel)
-	
-		if(args['id']=="start"):
-			sublime.message_dialog("cd "+enjoy+"\\rn"+" && "+"react-native start")
-			os.system("cd "+enjoy+"/rn"+" && "+"react-native start")
 
-		if(args['id']=="build" and not args['value']=="h5"):
-			sublime.message_dialog(args['id']+"===="+args['value'])
-			os.system("cd "+enjoy+""+" && "+"enjoy build --rn")
+		if "file_path" in dirs:	
+			dir1 = dirs['file_path']
+			enjoy = checkenjoy(dir1)
+			if(enjoy and args['id']=="start"):
+				os.system("cd "+enjoy+"/rn"+" && "+"react-native start")
 
-		if(args['id']=="build" and args['value']=="h5"):
-			sublime.message_dialog("cd "+enjoy+""+" && "+"enjoy build --web && cd web && webpack")
-			print("cd "+enjoy+""+" && "+"enjoy build --web && cd web && webpack")
-			os.system("cd "+enjoy+""+" && "+"enjoy build --web && cd web && webpack")
+			if(enjoy and  args['id']=="build" and not args['value']=="h5"):
+				os.system("cd "+enjoy+""+" && "+"enjoy build --rn")
+
+			if(enjoy and args['id']=="build" and args['value']=="h5"):
+				print("cd "+enjoy+""+" && "+"enjoy build --web && cd web && webpack")
+				os.system("cd "+enjoy+""+" && "+"enjoy build --web && cd web && webpack")
+
+			if(enjoy and  args['id']=="run"):
+				print("cd "+enjoy+""+" && "+"enjoy run --"+args['value'])
+				os.system("cd "+enjoy+""+" && "+"enjoy run --"+args['value'])
+		else:
+			if (not args['id']=="init"):
+				sublime.message_dialog("当前文件不属于enjoy项目")
